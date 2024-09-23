43e043074acfb0efce5fb49e801ca580c53d75a5
Kenneth Esguerra <kfesguerra@shakeys.biz>
2024-09-23 21:02:52 +0800

    Fix lint 2

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,8 +1,6 @@
 
 import time
-
 from psycopg2 import OperationalError as Psycopg20pError
-
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
@@ -17,5 +15,4 @@ class Command(BaseCommand):
             except (Psycopg20pError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-
         self.stdout.write(self.style.SUCCESS('Database available!'))
